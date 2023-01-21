16b184b2e83530b98dfb957f8125d7cc479a6115
Daiki008 <kindai0811@gmail.com>
2023-01-21 04:05:56 +0000

    フォロー機能実装

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #get 'relationships/create'
+  #get 'relationships/destroy'
   #get 'favorites/create'
   #get 'favorites/destroy'
   root 'tweets#index'
@@ -14,7 +16,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
 
-  resources :users
+  resources :users do 
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
